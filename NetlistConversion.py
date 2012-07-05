3535b678c58739459b0eee891359367a9912f190
Gabe Ferencz <gabe.public@gmail.com>
2012-07-04 21:18:11 -0400

    Changes to improve importability
    Added Netlist class that collects the part and net dictionaries, rather
    than have the static class variable dictionaries (because that breaks
    some things when the module is imported).

--- NetlistConversion.py
+++ NetlistConversion.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python
 # Gabe Ferencz
-import sys
 import time
 
-class Part(object):
-    parts = dict()
+class Netlist(object):
+    def __init__(self):
+        self.parts = dict()
+        self.nets = dict()
 
+    def parse_allegro(self, filename):
+        # A comma denotes a continued line, so let's join those lines
+        self.netlist = open(filename,'U').read().replace(',\n',' ')
+        childClass = None
+        for line in self.netlist.split('\n'):
+            if line.startswith('$PINS') or line.startswith('$A_PROPERTIES'):
+                childClass = None
+            elif line.startswith('$NETS'):
+                print('Parsing nets...')
+                childClass = Net
+            elif line.startswith('$PACKAGES'):
+                print('Parsing packages...')
+                childClass = Part
+            elif line == '':
+                pass
+            else:
+                if childClass is not None:
+                    childClass.parse_allegro(line, self)
+
+    def pads_output(self, filename):
+        outputData = '!PADS-POWERPCB-V3.0-MILS !Pads Power PCB Converted file '
+        outputData += time.strftime('%Y-%b-%d %H-%M')
+        outputData += '\n\n*PART*\n\n' + \
+                ''.join([part.pads_format() for part in self.parts.values()])
+        outputData += '\n\n*NET*\n\n' + \
+                ''.join([net.pads_format() for net in self.nets.values()])
+
+        outfile = open(filename, 'w')
+        outfile.write(outputData)
+        outfile.close()
+
+class Part(object):
     def __init__(self, refDes = '', footprint = '', partNumber = '',
                         value = '', tolerance = ''):
         self.footprint = footprint
@@ -21,7 +54,7 @@ class Part(object):
         return self.refDes + ' ' + self.partNumber + '\n'
 
     @staticmethod
-    def parse_allegro(line):
+    def parse_allegro(line, parent):
         # sample:
         # 'IPC_RES_SMT_0603H1P2' ! '803Wxxxxx' ! '75' ! '  1%' ;  R61 R60
         details = line.split(' ; ')[0].split(' ! ')
@@ -29,16 +62,9 @@ class Part(object):
         # A single line defines one or more instances of a part
         instances = line.split(' ; ')[1].split()
         for instance in instances:
-            Part.parts.update({instance:Part(instance,*details)})
-
-    @staticmethod
-    def pads_output():
-        return '\n\n*PART*\n\n' + \
-                ''.join([part.pads_format() for part in Part.parts.values()])
+            parent.parts.update({instance:Part(instance,*details)})
 
 class Net(object):
-    nets = dict()
-
     def __init__(self,netName,nodes):
         self.netName = netName
         self.nodes = nodes.split()
@@ -51,53 +77,20 @@ class Net(object):
         return '*signal* '+self.netName + '\n' + ' '.join(self.nodes) + '\n\n'
 
     @staticmethod
-    def parse_allegro(line):
+    def parse_allegro(line, parent):
         # sample:
         # 'CONFIG_OUT' ; R7.2 R76.1 R83.2 U1.78
         netName = line.split(' ; ')[0].strip("'")
         nodes = line.split(' ; ')[1]
-        return Net.nets.update({netName:Net(netName,nodes)})
-
-    @staticmethod
-    def pads_output():
-        return '\n\n*NET*\n\n' + \
-                ''.join([net.pads_format() for net in Net.nets.values()])
-
-def convert_allegro_to_pads(filename):
-    # A comma denotes a continued line, so let's join those lines with spaces
-    netlist = open(filename,'U').read().replace(',\n',' ')
-    parse_allegro(netlist)
-    # Create the Pads ascii file
-    outputData = '!PADS-POWERPCB-V3.0-MILS  !Pads Power PCB Converted file '
-    outputData += time.strftime('%Y-%b-%d %H-%M')
-    outputData += Part.pads_output()
-    outputData += Net.pads_output()
-    
-    outfile = open(filename[:-4]+'_Pads.asc','w')
-    outfile.write(outputData)
-    outfile.close()
-
-def parse_allegro(netlist):
-    currClass = None
-    for line in netlist.split('\n'):
-        if line.startswith('$PINS') or line.startswith('$A_PROPERTIES'):
-            currClass = None
-        elif line.startswith('$NETS'):
-            print('Parsing nets...')
-            currClass = Net
-        elif line.startswith('$PACKAGES'):
-            print('Parsing packages...')
-            currClass = Part
-        elif line == '':
-            pass
-        else:
-            if currClass is not None:
-                currClass.parse_allegro(line)
+        parent.nets.update({netName:Net(netName,nodes)})
 
 if __name__ == "__main__":
+    import sys
     filenames = sys.argv[1:]
     for filename in filenames:
         print(filename)
         if filename.endswith('.tel'):
-            convert_allegro_to_pads(filename)
+            netlist = Netlist()
+            netlist.parse_allegro(filename)
+            netlist.pads_output(filename[:-4]+'_Pads.asc')
     print('Conversion complete...')
